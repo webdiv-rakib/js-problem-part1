ce2b49ec8f5f62b1bb19074653dd17310db2ecaf
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-09 22:32:48 +0600

    convertion math

---
+++ convert.js
@@ -0,0 +1,7 @@
+// 12 inch 1 feet
+function inchToFeet(inch) {
+    const feet = inch / 12;
+    return feet;
+}
+const rakibsHeight = inchToFeet(66);
+console.log("Your height is:",rakibsHeight,'ft');
