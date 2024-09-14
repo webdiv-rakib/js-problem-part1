ab5f40bd4a8239dc374437e0d67b482568650bbc
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-14 20:09:18 +0600

    premade js math method

---
+++ math.js
@@ -0,0 +1,25 @@
+// finding out lowest number
+const min = Math.min(1, 3, 4, 7, 9, -1);
+console.log(min);
+// finding out highest number
+const max = Math.max(1, 3, 4, 7, 9, -1);
+console.log(max);
+// PI value
+console.log(Math.PI);
+// finding out difference between 2 numbers
+console.log(Math.abs(-88));
+/**
+ * Most use math method
+ */
+// this will give round numbers purno shongkha.
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+// floor means ground 
+console.log(Math.floor(2.95));
+// ceil means top value
+console.log(Math.ceil(2.1));
+// to generate random numbers
+console.log(Math.random() * 10000);
+// to make random number into purno shongkha
+const rand = Math.random() * 10000;
+console.log(Math.round(rand));
