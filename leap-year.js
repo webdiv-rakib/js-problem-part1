73b7d5cb132cc1c801545ea9f0440ed7bd6c22a6
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-14 18:11:02 +0600

    leap year method 2

--- leap-year.js
+++ leap-year.js
@@ -1,7 +1,7 @@
 /**
  * year will be a leap year if the year is divisible by 4
  */
-
+// simple logic.
 function isLeapYear(year) {
     if (year % 4 === 0) {
         return true;
@@ -11,4 +11,24 @@ function isLeapYear(year) {
     }
 }
 const isLipi = isLeapYear(2024);
-console.log(isLipi);
+// console.log(isLipi);
+
+/**
+ * 1. those year that is not divisble by 100, if the year is divisble by 4
+ *     then it will be a leap year.
+ * 2. 
+ */
+
+function isLeapYear2(year) {
+    if (year % 100 !== 0 && year % 4 === 0) {
+        return true;
+    }
+    else if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+const isLipi2 = isLeapYear2(2000);
+console.log(isLipi2);
